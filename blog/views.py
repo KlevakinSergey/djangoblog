6dd04b6da9789e737d4c64f983d373d1e89b3b03
KlevakinSergey <klevakin.sergey@gmail.com>
2020-01-27 16:04:28 +0600

    blog with mixin

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,47 @@ from .models import Post, Tag
 from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
 from .forms import TagForm, PostForm
 from django.urls import reverse
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', {'posts': posts})
+    search_query = request.GET.get('search', '')
+    
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+            
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+    
+    is_paginated = page.has_other_pages()
+    
+    if page.has_previous():
+        prev_url= f'?page={page.previous_page_number()}'
+
+    else:
+        prev_url = ''    
+
+    if page.has_next():
+        next_url = f'?page={page.next_page_number()}'
+
+    else:
+        next_url = ''
+
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url
+    }
+
+    return render(request, 'blog/index.html', context=context)
+    
+
 
 def tags_list(request):
     tags = Tag.objects.all()
@@ -30,50 +66,58 @@ class TagDetail(View):
 
 
 
-class PostUpdate(ObjectUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'blog/post_update.html'
+    raise_exception =True
 
 
 
 
-class PostCreate(ObjectCreateMixin,View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin,View):
     model_form = PostForm
     template = 'blog/post_create.html'
+    raise_exception =True
 
  
 
 
-class PostDelete(ObjectDeleteMixin, View):
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Post
     template = 'blog/post_delete.html'
     redirect_url = 'post_list_url'
+    raise_exception =True
 
 
     
 
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
 
     model_form = TagForm
     template = 'blog/tag_create.html'
+    raise_exception =True
 
      
         
 
-class TagUptate(ObjectUpdateMixin ,View):
+class TagUptate(LoginRequiredMixin, ObjectUpdateMixin ,View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update.html'
+    raise_exception =True
    
 
 
 
-class TagDelete(ObjectDeleteMixin ,View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin ,View):
     model = Tag
     template = 'blog/tag_delete.html'
     redirect_url = 'tags_list_url'
+    raise_exception =True
+
+
 
 
 
--- blogengine/urls.py
+++ blogengine/urls.py
@@ -19,9 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
